0ae6d3b4e82ac06fd6f6d188179541016c242a23
Merab <merab.tevdorashvili@gmail.com>
2022-04-10 22:42:26 +0400

    auth plug in added. and some widgets to decide which to use

--- lib/authentication/authentication.dart
+++ lib/authentication/authentication.dart
@@ -9,9 +9,53 @@ class Auth extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Authentication')),
-      body: const SafeArea(
-        child: Center(
-          child: Text('Auth Screen'),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('xdsds'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              child: const Text('sdsds'),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(
+              child: const Text('dfdfd'),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              child: const Text('dsds'),
+              onPressed: () {},
+              color: Colors.green,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RawMaterialButton(
+              fillColor: Colors.green,
+              child: const Text('dsdsds'),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CupertinoButton(
+              color: Colors.green,
+              child: const Text('dsdsd'),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
