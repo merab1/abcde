1f436f97b1daf2665fe9ea92790a674931fba04d
Merab <merab1>
2022-04-12 13:58:04 +0400

    two row back together, codeSent added

--- lib/authentication_screen/phone_login.dart
+++ lib/authentication_screen/phone_login.dart
@@ -3,6 +3,11 @@ import 'package:abcde/authentication_screen/sms_code.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+enum MobileVerificationState {
+  SHOW_MOBILE_FORM_STATE,
+  SHOW_OTP_FORM_STATE,
+}
+
 class PhoneLogin extends StatefulWidget {
   const PhoneLogin({Key? key}) : super(key: key);
   static const String pathId = 'PhoneLogin';
@@ -12,11 +17,15 @@ class PhoneLogin extends StatefulWidget {
 }
 
 class _PhoneLoginState extends State<PhoneLogin> {
+  MobileVerificationState currentState =
+      MobileVerificationState.SHOW_MOBILE_FORM_STATE;
   String _phoneNumber = '';
+  String _smsCode = '';
 
   final _auth = FirebaseAuth.instance;
 
   bool _isLoading = false;
+  String verificationId = '';
 
   @override
   Widget build(BuildContext context) {
@@ -43,9 +52,6 @@ class _PhoneLoginState extends State<PhoneLogin> {
                   const SizedBox(
                     height: 20,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
                   MaterialButton(
                     onPressed: () async {
                       if (_phoneNumber == '') {
@@ -59,23 +65,75 @@ class _PhoneLoginState extends State<PhoneLogin> {
                         });
                       }
                       try {
-                        final user = await _auth.verifyPhoneNumber(
+                        await _auth.verifyPhoneNumber(
                           phoneNumber: _phoneNumber,
                           codeAutoRetrievalTimeout: (String verificationId) {},
                           verificationCompleted:
-                              (PhoneAuthCredential phoneAuthCredential) {},
+                              (PhoneAuthCredential phoneAuthCredential) async {
+                            await _auth
+                                .signInWithCredential(phoneAuthCredential);
+                          },
                           codeSent: (String verificationId,
-                              int? forceResendingToken) {},
-                          verificationFailed: (FirebaseAuthException error) {},
+                              int? forceResendingToken) async {
+                            String smsCode = _smsCode;
+                            PhoneAuthCredential credential =
+                                PhoneAuthProvider.credential(
+                                    verificationId: verificationId,
+                                    smsCode: smsCode);
+                            await _auth.signInWithCredential(credential);
+                          },
+                          verificationFailed: (FirebaseAuthException error) {
+                            if (error.code == 'invalid-phone-number') {
+                              print('The provided phone number is not valid.');
+                            }
+                          },
                         );
 
-                          Navigator.pushNamed(context, SmsCode.pathId);
-
+                        Navigator.pushNamed(context, SmsCode.pathId);
                       } catch (e) {
                         print(e);
                       }
                     },
-                    child: const Text('Submit'),
+                    child: const Text('Verify'),
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter your  sms code'),
+                    onChanged: ((text) {
+                      _smsCode = text;
+                    }),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  MaterialButton(
+                    onPressed: () async {
+                      if (_smsCode == '') {
+                        const snackBar =
+                            SnackBar(content: Text('Please, enter SMS Code'));
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      } else {
+                        setState(() {
+                          _isLoading = true;
+                          print(_smsCode);
+                        });
+                      }
+                      try {
+                        final user =
+                            await _auth.signInWithPhoneNumber(_smsCode);
+                        if (user != null) {
+                          Navigator.pushNamed(context, AfterAuth.pathId);
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                    child: const Text('Login'),
                     color: Colors.blue,
                   ),
                 ],
